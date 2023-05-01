5704b6c41e7642a834bb4a9c7bb934b8634128bb
Nitesh Kumar <niteshkumarranjan534@gmail.com>
2023-05-01 13:13:49 +0530

    Login_PHP

---
+++ connection.php
@@ -0,0 +1,13 @@
+<?php
+$srvername="localhost";
+$username="root";
+$password="";
+$db_name="login";
+$conn= new mysqli($srvername, $username, $password, $db_name);
+if($conn->connect_error){
+    die("Connection failed".$conn->connect_error);
+}
+else{
+echo "";
+}
+?>
